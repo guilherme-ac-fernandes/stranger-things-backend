6158d65d6c6444b00d129a7fa3603cbc86db6ad8
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-06 17:39:40 -0300

    refactor: adiciona um console.log no services para avaliar as variáveis de ambiente

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 const PORT = process.env.PORT || 3000;
 
 const { UPSIDEDOWN_MODE } = process.env;
+console.log(UPSIDEDOWN_MODE);
 
 const app = express();
 
